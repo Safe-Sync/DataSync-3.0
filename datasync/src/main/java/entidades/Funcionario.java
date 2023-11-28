7275a04bea7227a76eb4a91906086019713fd7f6
DanielRRios <daniel.rios@sptech.school>
2023-11-28 15:02:37 -0300

    Logs refeitos

--- datasync/src/main/java/entidades/Funcionario.java
+++ datasync/src/main/java/entidades/Funcionario.java
@@ -1,10 +1,12 @@
 package entidades;
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import conexao.Conexao;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.util.Scanner;
 
 public class Funcionario {
     Conexao conectar = new Conexao();
@@ -17,21 +19,22 @@ public class Funcionario {
     private Integer fkEmpresa;
 
     public Funcionario(String email, String senha) {
-         try {
-             String sql = "SELECT idFuncionario, nomeFuncionario, email, fkEmpresa FROM funcionarios WHERE email = ? AND senha = ?";
-             con.queryForObject(sql, new Object[]{email, senha}, (rs,rowN) -> {
-                 this.idFuncionario = rs.getInt(1);
-                 this.nome = rs.getString(2);
-                 this.email = rs.getString(3);
-                 this.fkEmpresa = rs.getInt(4);
-                 return null;
-             });
+        try {
+            String sql = "SELECT idFuncionario, nomeFuncionario, email, fkEmpresa FROM funcionarios WHERE email = ? AND senha = ?";
+            con.queryForObject(sql, new Object[]{email, senha}, (rs, rowN) -> {
+                this.idFuncionario = rs.getInt(1);
+                this.nome = rs.getString(2);
+                this.email = rs.getString(3);
+                this.fkEmpresa = rs.getInt(4);
+                return null;
+            });
 
-             sistema.setFuncAtual(this.email, this.fkEmpresa, this.idFuncionario);
-             sistema.mensagemLoginValido(this.nome);
-         } catch (EmptyResultDataAccessException e) {
-             sistema.mensagemLoginInvalido();
-         }
+            sistema.setFuncAtual(this.email, this.fkEmpresa, this.idFuncionario);
+            sistema.mensagemLoginValido(this.nome);
+        } catch (EmptyResultDataAccessException e) {
+            sistema.mensagemLoginInvalido();
+            criarLogErroBancoDados(email + " " + "Erro ao tentar fazer login: " + e.getMessage());
+        }
     }
 
     public Integer getIdFuncionario() {
@@ -76,5 +79,25 @@ public class Funcionario {
                 email: %s
                 }
                 """, idFuncionario, fkEmpresa, nome, email);
+    }
+
+
+
+    public void criarLogErroBancoDados(String mensagem) {
+        String nomeArquivo = "errosLogin.txt";
+        String diretorio = System.getProperty("user.home") + "/Desktop/Logs/";
+
+        try {
+            File diretorioLogs = new File(diretorio);
+            if (!diretorioLogs.exists()) {
+                diretorioLogs.mkdirs();
+            }
+
+            FileWriter writer = new FileWriter(diretorio + nomeArquivo, true);
+            writer.write(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) + " - " + mensagem + "\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
